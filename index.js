345eab44d61ba39ae9856496b3fb6e73bee2769c
Paige Busch <93paigedf@gmail.com>
2017-02-21 14:48:58 -0700

    add messages

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var messages = require('./messages');
 
+var app = express();
+var port = 3000;
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+app.get('/messages', function(req, res, next) {
+    res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function(req, res, next) {
+    console.log(req.body);
+    messages.push({ message: req.body.message, time: new Date() });
+    res.status(200).json({ messages: messages });
+});
+
+app.listen(port, function() {
+    console.log('Listening on port ', port);
+});
